33e58d1babc962fac92d3f6e8caa409bcd71c985
CatsAndSecrets <theodorediamondTD@gmail.com>
2024-05-31 19:18:39 -0400

    YES SPINNYBOI IMPORT!!!

--- SCRIPZ/transparencyand3dlike3dshome.py
+++ SCRIPZ/transparencyand3dlike3dshome.py
@@ -4,80 +4,81 @@ import win32con
 import win32gui
 import time
 
+##no longer transparent. purely for spinning. i do not hav any idea why i decided for them to be together in the first place. probbably lazyness honestly
 
+screen = pygame.display.set_mode((876, 700), pygame.NOFRAME) # For borderless, use pygame.NOFRAME
 pygame.init()
-screen = pygame.display.set_mode((800, 600), pygame.NOFRAME) # For borderless, use pygame.NOFRAME
+
 done = False
 fuchsia = (255, 0, 128)  # Transparency color
 dark_red = (139, 0, 0)
 coler = (24, 0, 0)
 clock = pygame.time.Clock()
-image = pygame.image.load("C:\\Users\\tdthe\Pictures\\2021_07_10_the-whole-circusv2-18360095.png").convert_alpha()
+#image = pygame.image.load("C:\\Users\\tdthe\Pictures\\2021_07_10_the-whole-circusv2-18360095.png").convert_alpha() # make use local imkage to the project
 # Create layered window
-hwnd = pygame.display.get_wm_info()["window"]
-win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
-                       win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
+
+
+#hwnd = pygame.display.get_wm_info()["window"]
+#win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE,
+#                       win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE) | win32con.WS_EX_LAYERED)
 # Set window transparency color
-win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
-z = 1
+#win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LWA_COLORKEY)
+
 angle = 1
 angledirection = 1
-flip = False
 angletr = 1
 
-skippin = 4 #how much of a step to take each new render of the animation. really much, the bigger this is, the chunkier the animation. Nice. its a good thing. i want that.
+skippin = 1 #how much of a step to take each new render of the animation. really much, the bigger this is, the chunkier the animation. Nice. its a good thing. i want that.
 
 #todo, add random flickering, or not. ill probably include that as a thing in the main thing over everything
-
-def run():
-    global angle, angletr, angledirection, z
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        x, y = pygame.mouse.get_pos()
-    for x in range(0, skippin):
-        if (angletr+skippin*angledirection) > 90 or (angletr+skippin*angledirection) < 0:
-            print("")
-            angledirection = angledirection*-1
+class spinnerimageboi():
+    def run(imagelocation, xs, ys, width, height):
+        global angle, angletr, angledirection
+        image = pygame.image.load(imagelocation).convert_alpha()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                x, y = pygame.mouse.get_pos()
+        for x in range(0, skippin):
+            if (angletr+skippin*angledirection) > 90 or (angletr+skippin*angledirection) < 0:
+                print("")
+                angledirection = angledirection*-1
         angletr += angledirection*skippin
         angle += skippin 
         angle = angle%360
-        #angledr = angle%90
-        z = (z+1)%100
-    print(angletr)
-    screen.fill(dark_red)
-    #Imaged = pygame.transform.smoothscale(image, (int(((90 - angledr)/90) * image.get_size()[0]),175))
+      
+        print(angletr)
     
     
-    if 1 < angle and angle < 90:
-        Imaged = pygame.transform.smoothscale(image, (int(((90 - angletr)/90) * 175),175))
-        for i in range(round((angletr/10*-1)), round((angletr/10))): 
-            screen.blit(pygame.transform.flip(Imaged, False, False), pygame.Rect(100+angletr+i, 100, 175, 175))
+        if 1 < angle and angle < 90:
+            Imaged = pygame.transform.smoothscale(image, (int(((90 - angletr)/90) * width),height))
+            for i in range(round((angletr/10*-1)), round((angletr/10))): 
+                screen.blit(pygame.transform.flip(Imaged, False, False), pygame.Rect(xs+angletr+i, ys, width, height))
             
-    elif 90 < angle and angle < 180:
-        Imaged = pygame.transform.smoothscale(image, (int(((90 - angletr)/90) * 175),175))
-        for i in range(round((angletr/10*-1)), round((angletr/10))): 
-            screen.blit(pygame.transform.flip(Imaged, True, False), pygame.Rect(100+angletr-i, 100, 175, 175))
+        elif 90 < angle and angle < 180:
+            Imaged = pygame.transform.smoothscale(image, (int(((90 - angletr)/90) * width),height))
+            for i in range(round((angletr/10*-1)), round((angletr/10))): 
+                screen.blit(pygame.transform.flip(Imaged, True, False), pygame.Rect(xs+angletr-i, ys, width, height))
         
 
-    elif 180 < angle and angle < 270:
-        Imaged = pygame.transform.smoothscale(image, (int(((90 - angletr)/90) * 175),175))
-        for i in range(round((angletr/10*-1)), round((angletr/10))): 
-            screen.blit(pygame.transform.flip(Imaged, True, False), pygame.Rect(100+angletr+i, 100, 175, 175))
+        elif 180 < angle and angle < 270:
+            Imaged = pygame.transform.smoothscale(image, (int(((90 - angletr)/90) * width),height))
+            for i in range(round((angletr/10*-1)), round((angletr/10))): 
+                screen.blit(pygame.transform.flip(Imaged, True, False), pygame.Rect(xs+angletr+i, ys, width, height))
             
-    elif 270 < angle and angle < 360:
-        Imaged = pygame.transform.smoothscale(image, (int(((90 - angletr)/90) * 175),175))
-        for i in range(round((angletr/10*-1)), round((angletr/10))): 
-            screen.blit(pygame.transform.flip(Imaged, False, False), pygame.Rect(100+angletr-i, 100, 175, 175)) 
+        elif 270 < angle and angle < 360:
+            Imaged = pygame.transform.smoothscale(image, (int(((90 - angletr)/90) * width),height))
+            for i in range(round((angletr/10*-1)), round((angletr/10))): 
+                screen.blit(pygame.transform.flip(Imaged, False, False), pygame.Rect(xs+angletr-i, ys, width, height)) 
             
 
-    pygame.display.update()
+        #pygame.display.update()
     
-    clock.tick(12)
-
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-    run()
+        #clock.tick(12)
+    
+    #while not done:
+     #   for event in pygame.event.get():
+      #      if event.type == pygame.QUIT:
+       #         done = True
+    #run()
