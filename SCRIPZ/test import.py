05a7b22425ff8b08e8fcc079c5650ed34de01961
CatsAndSecrets <theodorediamondTD@gmail.com>
2024-06-04 20:08:06 -0400

    a

--- SCRIPZ/test import.py
+++ SCRIPZ/test import.py
@@ -9,6 +9,7 @@ screen = pygame.display.set_mode((876, 700), pygame.NOFRAME) # For borderless, u
 done = False
 runn = 0
 img = pygame.image.load("C:\\Users\\tdthe\Pictures\\2021_07_10_the-whole-circusv2-18360095.png").convert_alpha()
+numbr = 1
 while not done:
     screen.fill(dark_red)
     
@@ -21,7 +22,9 @@ while not done:
                 #spinnerimageboi.run("C:\\Users\\tdthe\Pictures\\2021_07_10_the-whole-circusv2-18360095.png", 100, 100, 175, 175)
   #  keys=pygame.key.get_pressed()  
    # if keys[pygame.K_s]:
-    spinnerimageboi.run("C:\\Users\\tdthe\Pictures\\2021_07_10_the-whole-circusv2-18360095.png", 100, 100, 175, 175)
+    #print(f"C:\\Users\\tdthe\Downloads\ezgif-1-7bb1fff934-gif-png\\ ({numbr})).png")
+    spinnerimageboi.run(f"C:\\Users\\tdthe\Downloads\ezgif-1-7bb1fff934-gif-png\\ ({numbr}).png", 100, 100, 175, 175)
+    numbr = numbr % 39 +1
     #if runn == 1:
         #end = 
         #if int(time.time()) - int(start) > 10:
